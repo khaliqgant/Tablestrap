df37ffb51127c891eea274451c8552c947c08d89
khaliqgant <khaliqgant@gmail.com>
2012-06-11 22:33:17 -0400

    updating table class, adding to render class

--- table.class.php
+++ table.class.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Class to create tables from an array
- * @oauthor Khaliq
+ * @oa Khaliq
  */
 
 namespace tablestrap;
@@ -11,20 +11,44 @@ class table{
      * table class can be as defined by Bootstrap default, striped, bordered, condensed, or a combination of these
      */
     private $table_class;
+    private $data, $key, $value;
+
+
 
     /*
      * Constructor
      * @oa Khaliq
-     * table_type is the table class types
+     * $data is the array of information to create the table
      */
 
-    public function _construct($table_type = 'default')
+    public function __construct($table_class = 'default', $data = array())
     {
-        $this->
+        $this->table_class = $table_class;
+        $this->data
+
     }
 
 
 
+    public function render()
+    {
 
-
+        $html   = '<table class="' . $this->table_class . '">';
+            foreach ($data[0] as $key=>$value){
+        $html  .= '<thead><th>"' . $key. '"</th></thead>';
+            }
+            foreach ($data as $value['id']){
+                $html   .= '<tr><td>"' .$value. '"</td></tr>';
+            }
+        $html  .= '</table>';
+        return $html;
+    }
 }
+
+
+
+
+
+
+
+
